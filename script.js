21010f1b95df8f6080d294c6b768de3aef9bac1c
ninalima-tech <ninafloreslima@gmail.com>
2024-12-02 14:25:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function adicionarTarefa() {
+    const tarefa = {
+      nome: document.getElementById('nomeTarefa').value,
+      dataVencimento: document.getElementById('dataVencimento').value,
+      responsavel: document.getElementById('responsavel').value
+    };
+  
+    let tarefas
+  
+  }
